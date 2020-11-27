c641cbd1bc32c3498c4884c9dab3d0228b5c1c0a
mariusz <mariuszjurczenko@gmail.com>
2020-11-27 12:30:57 +0100

    Dodaj pliki projektów.

---
+++ CsharpKillSystem/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CsharpKillSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 1000000; i++)
+            {
+                new Person() { Id = i, Name = i.ToString(), Email = i.ToString() };
+
+                if (i % 100000 == 0)
+                {
+                    Console.WriteLine(GetTotalCollections());
+                }
+            }
+        }
+
+        static int GetTotalCollections()
+        {
+            return GC.CollectionCount(0) +
+                   GC.CollectionCount(1) +
+                   GC.CollectionCount(2);
+        }
+
+        static List<Person> people = new List<Person>();
+    }
+}
